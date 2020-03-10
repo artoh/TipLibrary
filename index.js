af3aa120822651d426604ec401f6019e02d57054
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2020-03-10 08:29:02 +0200

    Server initial commit
    Server will respond to /api/tips with static content. Directory structure not ready.

---
+++ index.js
@@ -0,0 +1,24 @@
+ 
+const express = require('express')
+const app = express()
+
+
+const tips = [
+    {
+        title: "Full stack-kurssi",
+        link: "https://fullstackopen.com"
+    },
+    {
+        title: "DevOps-kurssi",
+        link: "https://devopswithdocker.com/"
+    }
+]
+
+app.get('/api/tips', (req,res) => {
+    res.send(tips)
+})
+
+const port = process.env.APIPORT == undefined ? 3001 : process.env.APIPORT
+app.listen(port, () => {
+    console.log(`TipLibrary Server running on port ${port}`)
+})
