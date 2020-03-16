01b0b13c672708916ffa0a2bbd3f9e2329b6fee2
Ilari Junnonen <ilari.junnonen@iki.fi>
2020-03-16 19:50:03 +0200

    Add axios update function and test

--- frontend/src/services/tips.js
+++ frontend/src/services/tips.js
@@ -1,18 +1,24 @@
 import axios from 'axios'
 
-const API = '/api/tips'
+const apiUrl = '/api/tips'
 
 const getAll = () => {
-    const request = axios.get(API)
+    const request = axios.get(apiUrl)
     return request.then(response => response.data)
 }
 
 const create = newTip => {
-    const request = axios.post(API, newTip)
+    const request = axios.post(apiUrl, newTip)
+    return request.then(response => response.data)
+}
+
+const update = (id, editedTip) => {
+    const request = axios.put(`${apiUrl}/${id}`, editedTip)
     return request.then(response => response.data)
 }
 
 export default {
     getAll: getAll,
-    create: create
+    create: create,
+    update: update
 }
--- frontend/src/services/tips.test.js
+++ frontend/src/services/tips.test.js
@@ -1,39 +1,48 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import db from './tips';
+import tipService from './tips';
 
 
 describe('tips', () => {
 
     let mock = new MockAdapter(axios);
 
-    it('returns response when create() is called', done => {
-        const newTip = { title: 'test tip', link: 'www.tip.com' };
+    let newTip = { id: '5e6fb7551ecf2116b88dc017', title: 'test tip 1', link: 'www.tip1.com' };
+    let newTip2 = { id: '5e69cfe35cc3450017e58280', title: 'test tip 2', link: 'www.tip2.com' };
 
+    let tipList = { 1: newTip, 2: newTip2 };
+
+
+    it('returns created tip when create() is successfully called', done => {
         mock.onPost('/api/tips').reply(201, newTip);
 
-        db.create(newTip).then(response => {
+        tipService.create(newTip).then(response => {
             expect(response).toEqual(newTip);
             done();
         });
     });
 
-    it('returns list of tips when getAll() is called', done => {
-        const tipList = {
-            1: {
-                title: 'test tip 1',
-                link: 'www.tip1.com'
-            },
-            2: {
-                title: 'test tip 2',
-                link: 'www.tip2.com'
-            }
-        };
-
+    it('returns list of tips when getAll() is succesfully called', done => {
         mock.onGet('/api/tips').reply(200, tipList);
 
-        db.getAll().then(response => {
+        tipService.getAll().then(response => {
             expect(response).toEqual(tipList);
+            done();
+        });
+    });
+
+    it('returns updated tip when update() is successfully called', done => {
+        let tipId = newTip.id;
+        let updatedTip = {
+            id: tipId,
+            title: "edit title",
+            link: "edit link"
+        };
+
+        mock.onPut(`/api/tips/${tipId}`).reply(200, updatedTip);
+
+        tipService.update(tipId, updatedTip).then(response => {
+            expect(response).toEqual(updatedTip);
             done();
         });
     });
