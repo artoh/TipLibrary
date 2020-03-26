9b0648e9aeba01945985cd112dbd079724b60061
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2020-03-26 13:12:57 +0200

    Remove dependency to chromedriver npm package from cucumber world

--- features/support/world.js
+++ features/support/world.js
@@ -1,14 +1,12 @@
 const { setWorldConstructor } = require("cucumber");
-require("chromedriver");
+//require("chromedriver");
 const seleniumWebdriver = require("selenium-webdriver")
-const chrome = require("selenium-webdriver/chrome");
-const chromium = require("chromium")
 
 function customWorld() {
 
-  const options = new chrome.Options()
-  options.setChromeBinaryPath(chromium.path)
-  this.driver = new seleniumWebdriver.Builder().forBrowser("chrome").setChromeOptions(options).build();
+  // const options = new chrome.Options()
+  //  o ptions.setChromeBinaryPath(chromium.path)//
+  this.driver = new seleniumWebdriver.Builder().forBrowser("chrome") //.setChromeOptions(options).build();
 
   this.waitForElement = function(locator) {
     const condition = seleniumWebdriver.until.elementLocated(locator);
